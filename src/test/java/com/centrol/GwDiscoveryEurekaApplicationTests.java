08ff346a2a4564a40edafffb798d16a3c80391e6
bennani <adil.bennani.ma@gmail.com>
2021-04-20 04:58:16 +0200

    first commit

---
+++ src/test/java/com/centrol/GwDiscoveryEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.centrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GwDiscoveryEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
